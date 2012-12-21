8b90c7279e22dd90ed957ff03a4d7601e5630df3
Dennis Rassmann <showp1984@gmail.com>
2012-12-21 03:25:36 +0100

    thermal: fix clear thresholds
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>
    
    Conflicts:
    	drivers/thermal/msm_thermal.c
    
    Change-Id: Icd5551bab4986f6bb307a5c340620b4a2725c225

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -248,6 +248,8 @@ static void check_temp(struct work_struct *work)
 		    ((max(temp0, temp1)) < msm_thermal_tuners_ins.allowed_mid_high) &&
 		    (cpu_policy->max > msm_thermal_tuners_ins.allowed_low_freq)) {
 			update_policy = 1;
+                    (thermal_throttled < 2)) {
+			update_policy = true;
 			max_freq = msm_thermal_tuners_ins.allowed_low_freq;
 			thermal_throttled = 1;
 			pr_warn("msm_thermal: Thermal Throttled (low)! temp:%lu by:%s\n",
@@ -277,6 +279,9 @@ static void check_temp(struct work_struct *work)
 			   ((max(temp0, temp1)) < msm_thermal_tuners_ins.allowed_mid_low) &&
 			   (cpu_policy->max > msm_thermal_tuners_ins.allowed_mid_freq)) {
 			update_policy = 1;
+			   ((max(temp0, temp1)) < msm_thermal_tuners_ins.allowed_max_high) &&
+			   (thermal_throttled < 2)) {
+			update_policy = true;
 			max_freq = msm_thermal_tuners_ins.allowed_low_freq;
 			thermal_throttled = 2;
 			pr_warn("msm_thermal: Thermal Throttled (mid)! temp:%lu by:%s\n",
@@ -300,6 +305,8 @@ static void check_temp(struct work_struct *work)
 		} else if (((max(temp0, temp1)) >= msm_thermal_tuners_ins.allowed_max_high) &&
 			   (cpu_policy->max > msm_thermal_tuners_ins.allowed_max_freq)) {
 			update_policy = 1;
+		} else if ((max(temp0, temp1)) >= msm_thermal_tuners_ins.allowed_max_high) {
+			update_policy = true;
 			max_freq = msm_thermal_tuners_ins.allowed_max_freq;
 			thermal_throttled = 3;
 			pr_warn("msm_thermal: Thermal Throttled (max)! temp:%lu by:%s\n",
@@ -321,7 +328,7 @@ static void check_temp(struct work_struct *work)
 			}
 		}
 
-		if (update_policy)
+		if ((update_policy) && (cpu_policy->max > max_freq))
 			update_cpu_max_freq(cpu_policy, cpu, max_freq);
 
 		cpufreq_cpu_put(cpu_policy);
