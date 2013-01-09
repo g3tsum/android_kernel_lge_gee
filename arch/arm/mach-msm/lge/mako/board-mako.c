3433899d111a8c8dc0c69c21b483585ed4e62360
Dennis Rassmann <showp1984@gmail.com>
2013-01-09 01:11:03 +0100

    thermal: update upstream msm_thermal with 3-phase control
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>
    
    Conflicts:
    	drivers/thermal/msm_thermal.c
    
    Conflicts:
    	arch/arm/mach-msm/lge/mako/board-mako.c
    	drivers/thermal/msm_thermal.c
    	include/linux/msm_thermal.h
    
    Change-Id: I5645f3a620b63590ccae09bb73df6cd086e9417a

--- arch/arm/mach-msm/lge/mako/board-mako.c
+++ arch/arm/mach-msm/lge/mako/board-mako.c
@@ -1451,6 +1451,20 @@ static struct msm_thermal_data msm_thermal_pdata = {
 	.freq_step = 2,
 	.core_temp_hysteresis_degC = 10,
 	.core_control_mask = 0xe,
+	.poll_ms = 100,
+	.shutdown_temp = 88,
+
+	.allowed_max_high = 84,
+	.allowed_max_low = 80,
+	.allowed_max_freq = 384000,
+
+	.allowed_mid_high = 81,
+	.allowed_mid_low = 76,
+	.allowed_mid_freq = 810000,
+
+	.allowed_low_high = 79,
+	.allowed_low_low = 73,
+	.allowed_low_freq = 1350000,
 };
 
 #define MSM_SHARED_RAM_PHYS 0x80000000
