57966e92e85e56f49a6a939f1d45c67779854b30
Devin Kim <dojip.kim@lge.com>
2013-02-05 16:19:36 -0800

    mako: msm: hsic: Disallow processor idle sleep while driving resume signal
    HSIC controller should send SOF with in 3 msec after completing
    the resume signal.  If processor is in idle sleep state, the timer
    interrupt generated by HSIC controller to indicate resume completion
    gets delayed.  If the interrupt handler is not run with in 3msec
    after resume, the resume sequence is repeated.  Disallow processor
    idle sleep to avoid multiple resume cycles.
    
    CRs-Fixed: 397154
    Change-Id: Ie2c6f60b3a36f46f8246e2e0f41a64675df632a5
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- arch/arm/mach-msm/lge/mako/board-mako.c
+++ arch/arm/mach-msm/lge/mako/board-mako.c
@@ -2082,8 +2082,11 @@ static void __init apq8064_common_init(void)
 	platform_add_devices(common_devices, ARRAY_SIZE(common_devices));
 	platform_add_devices(common_not_mpq_devices,
 			ARRAY_SIZE(common_not_mpq_devices));
-	apq8064_device_hsic_host.dev.platform_data = &msm_hsic_pdata;
-	device_initialize(&apq8064_device_hsic_host.dev);
+	if (!mako_charger_mode) {
+		msm_hsic_pdata.swfi_latency = msm_rpmrs_levels[0].latency_us;
+		apq8064_device_hsic_host.dev.platform_data = &msm_hsic_pdata;
+		device_initialize(&apq8064_device_hsic_host.dev);
+	}
 	apq8064_pm8xxx_gpio_mpp_init();
 	apq8064_init_mmc();
 
