098a44aefbf4f7b1d837e66572114ae28f957416
xboxfanj <xboxfanj@msn.com>
2013-04-30 01:36:10 -0500

    Fix merge conflicts

--- drivers/video/msm/msm_dss_io_8960.c
+++ drivers/video/msm/msm_dss_io_8960.c
@@ -704,13 +704,10 @@ void mipi_dsi_clk_enable(void)
 	if (clk_set_rate(dsi_byte_div_clk, 1) < 0)      /* divided by 1 */
 		pr_err("%s: dsi_byte_div_clk - "
 			"clk_set_rate failed\n", __func__);
-<<<<<<< HEAD
 	if (clk_set_rate(dsi_esc_clk, esc_byte_ratio) < 0) /* divided by esc */
 		pr_err("%s: dsi_esc_clk - "                      /* clk ratio */
-=======
 	if (clk_set_rate(dsi_esc_clk, 2) < 0) /* divided by 2 */
 		pr_err("%s: dsi_esc_clk - "
->>>>>>> parent of cf78301... Revert "mako: enable lgit lcd driver"
 			"clk_set_rate failed\n", __func__);
 	mipi_dsi_pclk_ctrl(&dsi_pclk, 1);
 	mipi_dsi_clk_ctrl(&dsicore_clk, 1);
