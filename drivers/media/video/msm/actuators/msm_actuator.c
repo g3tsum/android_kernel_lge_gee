85f902f0f2ca24004d880f8eeccc3c6699c1f3b3
xboxfanj <xboxfanj@msn.com>
2013-04-30 06:25:06 -0500

    Fix actuator conflict

--- drivers/media/video/msm/actuators/msm_actuator.c
+++ drivers/media/video/msm/actuators/msm_actuator.c
@@ -378,8 +378,6 @@ static int32_t msm_actuator_set_default_focus(
 static int32_t msm_actuator_power_down(struct msm_actuator_ctrl_t *a_ctrl)
 {
 	int32_t rc = 0;
-<<<<<<< HEAD
-=======
 #ifdef CONFIG_SEKONIX_LENS_ACT
 	int cur_pos = a_ctrl->curr_step_pos;
 	struct msm_actuator_move_params_t move_params;
@@ -392,7 +390,6 @@ static int32_t msm_actuator_power_down(struct msm_actuator_ctrl_t *a_ctrl)
 		msleep(300);
 	}
 #endif
->>>>>>> 39588ed... Add ifdef around SEKONIX_LENS_ACT specific actuator position in power
 	if (a_ctrl->vcm_enable) {
 		rc = gpio_direction_output(a_ctrl->vcm_pwd, 0);
 		if (!rc)
