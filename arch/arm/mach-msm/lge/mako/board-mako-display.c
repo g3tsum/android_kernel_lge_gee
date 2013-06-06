a7803c7ebcd63922744e15565dd16e02bf2c602e
xboxfanj <xboxfanj@msn.com>
2013-06-06 13:43:27 -0500

    Add CAF changes to LGE defconfigs and board files

--- arch/arm/mach-msm/lge/mako/board-mako-display.c
+++ arch/arm/mach-msm/lge/mako/board-mako-display.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
  * Copyright (c) 2012, LGE Inc.
  *
  * This program is free software; you can redistribute it and/or modify
@@ -273,6 +273,9 @@ void __init apq8064_mdp_writeback(struct memtype_reserve* reserve_table)
 		mdp_pdata.ov0_wb_size;
 	reserve_table[mdp_pdata.mem_hid].size +=
 		mdp_pdata.ov1_wb_size;
+
+  pr_info("mem_map: mdp reserved with size 0x%lx in pool\n",
+      mdp_pdata.ov0_wb_size + mdp_pdata.ov1_wb_size);
 #endif
 }
 
