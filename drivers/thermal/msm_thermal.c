282d3a72fa3eedd8b678dca27d3bf9534a885f6d
houstonn <houstonn@gmail.com>
2013-07-09 22:51:07 +0000

    drivers: thermal: fixup use custom msm_thermal driver in kernel

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -38,122 +38,19 @@ static struct msm_thermal_data msm_thermal_info;
 
 static struct workqueue_struct *wq;
 static struct delayed_work check_temp_work;
-static bool core_control_enabled;
-static uint32_t cpus_offlined;
-static DEFINE_MUTEX(core_control_mutex);
 
-static int limit_idx;
-static int limit_idx_low;
-static int limit_idx_high;
-static struct cpufreq_frequency_table *table;
-
-static int msm_thermal_get_freq_table(void)
-{
-	int ret = 0;
-	int i = 0;
-
-	table = cpufreq_frequency_get_table(0);
-	if (table == NULL) {
-		pr_debug("%s: error reading cpufreq table\n", KBUILD_MODNAME);
-		ret = -EINVAL;
-		goto fail;
-	}
-
-	while (table[i].frequency != CPUFREQ_TABLE_END)
-		i++;
-
-	limit_idx_low = 0;
-	limit_idx_high = limit_idx = i - 1;
-	BUG_ON(limit_idx_high <= 0 || limit_idx_high <= limit_idx_low);
-fail:
-	return ret;
-}
-
-static int update_cpu_max_freq(int cpu, uint32_t max_freq)
-{
-	int ret = 0;
-
-	ret = msm_cpufreq_set_freq_limits(cpu, MSM_CPUFREQ_NO_LIMIT, max_freq);
-	if (ret)
-		return ret;
-
-	limited_max_freq = max_freq;
-	if (max_freq != MSM_CPUFREQ_NO_LIMIT)
-		pr_info("%s: Limiting cpu%d max frequency to %d\n",
-				KBUILD_MODNAME, cpu, max_freq);
-	else
-		pr_info("%s: Max frequency reset for cpu%d\n",
-				KBUILD_MODNAME, cpu);
+struct cpufreq_policy *policy = NULL;
 
 unsigned int max_freq;
 unsigned int freq_buffer;
 
-	return ret;
+unsigned short get_threshold()
+{
+	return temp_threshold;
 }
 
-static void __cpuinit do_core_control(long temp)
+static void check_temp(struct work_struct *work)
 {
-	int i = 0;
-	int ret = 0;
-
-	if (!core_control_enabled)
-		return;
-
-	/**
-	 *  Offline cores starting from the max MPIDR to 1, when above limit,
-	 *  The core control mask is non zero and allows the core to be turned
-	 *  off.
-	 *  The core was not previously offlined by this module
-	 *  The core is the next in sequence.
-	 *  If the core was online for some reason, even after it was offlined
-	 *  by this module, offline it again.
-	 *  Online the back on if the temp is below the hysteresis and was
-	 *  offlined by this module and not already online.
-	 */
-	mutex_lock(&core_control_mutex);
-	if (msm_thermal_info.core_control_mask &&
-		temp >= msm_thermal_info.core_limit_temp_degC) {
-		for (i = num_possible_cpus(); i > 0; i--) {
-			if (!(msm_thermal_info.core_control_mask & BIT(i)))
-				continue;
-			if (cpus_offlined & BIT(i) && !cpu_online(i))
-				continue;
-			pr_info("%s: Set Offline: CPU%d Temp: %ld\n",
-					KBUILD_MODNAME, i, temp);
-			ret = cpu_down(i);
-			if (ret)
-				pr_err("%s: Error %d offline core %d\n",
-					KBUILD_MODNAME, ret, i);
-			cpus_offlined |= BIT(i);
-			break;
-		}
-	} else if (msm_thermal_info.core_control_mask && cpus_offlined &&
-		temp <= (msm_thermal_info.core_limit_temp_degC -
-			msm_thermal_info.core_temp_hysteresis_degC)) {
-		for (i = 0; i < num_possible_cpus(); i++) {
-			if (!(cpus_offlined & BIT(i)))
-				continue;
-			cpus_offlined &= ~BIT(i);
-			pr_info("%s: Allow Online CPU%d Temp: %ld\n",
-					KBUILD_MODNAME, i, temp);
-			/* If this core is already online, then bring up the
-			 * next offlined core.
-			 */
-			if (cpu_online(i))
-				continue;
-			ret = cpu_up(i);
-			if (ret)
-				pr_err("%s: Error %d online core %d\n",
-						KBUILD_MODNAME, ret, i);
-			break;
-		}
-	}
-	mutex_unlock(&core_control_mutex);
-}
-
-static void __cpuinit check_temp(struct work_struct *work)
-{
-	static int limit_init;
 	struct tsens_device tsens_dev;
 	long temp = 0;
     unsigned int cpu;
@@ -277,9 +174,3 @@ int __init msm_thermal_device_init(void)
 {
 	return platform_driver_register(&msm_thermal_device_driver);
 }
-
-int __init msm_thermal_late_init(void)
-{
-	return msm_thermal_add_cc_nodes();
-}
-module_init(msm_thermal_late_init);
