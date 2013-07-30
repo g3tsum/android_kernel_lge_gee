843ec40b4f62efd2683991ee14248067c312496f
Jake Weinstein <xboxfanj@msn.com>
2013-07-30 17:19:54 -0500

    Fix redefinition errors
    Change-Id: I52b93a69f6f0fe48d608efa015689b5f0fbe5166

--- include/linux/msm_thermal.h
+++ include/linux/msm_thermal.h
@@ -19,8 +19,6 @@
 struct msm_thermal_data {
 	uint32_t sensor_id;
 	uint32_t poll_ms;
-	int32_t limit_temp_degC;
-	int32_t temp_hysteresis_degC;
 	uint32_t freq_step;
 	int32_t core_limit_temp_degC;
 	int32_t core_temp_hysteresis_degC;
