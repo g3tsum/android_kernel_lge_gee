f0f700814e4f900b7fbcef3671e51b5bf5d5a916
Your Name <altabraydon@gmail.com>
2013-10-06 18:38:19 -0700

    Change elapsed_csecs to elapsed_msecs
    Signed-off-by: Braydon Saporito <altabraydon@gmail.com>

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -103,7 +103,7 @@ static int try_to_freeze_tasks(bool user_only)
 			printk(KERN_ERR "Freezing of tasks %s after %d.%02d seconds "
 			       "(%d tasks refusing to freeze, wq_busy=%d):\n",
 			       wakeup ? "aborted" : "failed",
-			       elapsed_csecs / 100, elapsed_csecs % 100,
+			       elapsed_msecs / 100, elapsed_msecs % 100,
 			       todo - wq_busy, wq_busy);
 		}
 
@@ -112,7 +112,7 @@ static int try_to_freeze_tasks(bool user_only)
 			do_each_thread(g, p) {
 				if (p != current && !freezer_should_skip(p)
 				    && freezing(p) && !frozen(p) &&
-				    elapsed_csecs > 100)
+				    elapsed_msecs > 100)
 					sched_show_task(p);
 			} while_each_thread(g, p);
 			read_unlock(&tasklist_lock);
